2147dff21af9db3f4c7f2ad675cd8db20cdb3753
Eli Wood <mreliwood1@gmail.com>
2024-02-23 23:09:31 -0800

    restructured export

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
+/* base ******************************************/
+/***** reset ****************************************/
+import './layers/reset.css';
+
+/***** colors ***************************************/
+/******** palette **************************************/
+import './layers/colors/palette/01-slate.css';
+import './layers/colors/palette/02-gray.css';
+import './layers/colors/palette/03-zinc.css';
+import './layers/colors/palette/04-neutral.css';
+import './layers/colors/palette/05-stone.css';
+import './layers/colors/palette/06-red.css';
+import './layers/colors/palette/07-orange.css';
+import './layers/colors/palette/08-amber.css';
+import './layers/colors/palette/09-yellow.css';
+import './layers/colors/palette/10-lime.css';
+import './layers/colors/palette/11-green.css';
+import './layers/colors/palette/12-emerald.css';
+import './layers/colors/palette/13-teal.css';
+import './layers/colors/palette/14-cyan.css';
+import './layers/colors/palette/15-sky.css';
+import './layers/colors/palette/16-blue.css';
+import './layers/colors/palette/17-indigo.css';
+import './layers/colors/palette/18-violet.css';
+import './layers/colors/palette/19-purple.css';
+import './layers/colors/palette/20-fuchsia.css';
+import './layers/colors/palette/21-pink.css';
+import './layers/colors/palette/22-rose.css';
+import './layers/colors/palette/index.css';
+
+import './layers/colors/primary.css';
+import './layers/colors/secondary.css';
+import './layers/colors/accent.css';
+import './layers/colors/compliment.css';
+import './layers/colors/index.css';
+
+/* theme *****************************************/
+import './layers/themeDefaults.css';
+
+/* superset **************************************/
+/***** sizes ****************************************/
+import './layers/sizes.css';
+
+/***** fonts ****************************************/
+import './layers/fonts.css';
+
+/***** animations ***********************************/
+import './layers/animations.css';
+
+/***** elements *************************************/
+import './layers/elements.css';
+
 import './layers/index.css';
